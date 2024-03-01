3ed2c20a004c11fc91317616ee4be5e19c67e395
Johnny Wesley da Silva Santos - External <e-johnnyws@weg.net>
2024-03-01 08:58:12 -0300

    added icon for cart

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -12,8 +12,11 @@ export default function NavBar() {
           Next Store
         </Link>
         <div className="flex items-center gap-8">
-            <div className="flex items-center cursor-pointer">
-                <svg></svg>
+            <div className="flex items-center cursor-pointer relative">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+</svg>
+<span className="bg-teal-600 text-sm font-bold rounded-full h-5 w-5 flex items-center justify-center absolute left-3 bottom-3">2</span>
             </div>
             <div>
             <SignedIn><UserButton/></SignedIn>
