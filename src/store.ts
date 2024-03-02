19434d9ffebfd68fab45b1120a949159493475bf
Johnny Santos <johnny.wesley@gmail.com>
2024-03-01 21:14:15 -0300

    first incremented products

--- src/store.ts
+++ src/store.ts
@@ -1,21 +1,44 @@
-import {create} from 'zustand';
+import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import { ProductType} from "./types/ProductType";
+import { ProductType } from "./types/ProductType";
 
 type CartState = {
-    cart: ProductType[],
-    addProduct:(product: ProductType) => void;
-    removeProduct:(productId: string) => void;
-    isOpen: boolean,
-    toggleCart: () => void;
-}
+  cart: ProductType[];
+  addProduct: (product: ProductType) => void;
+  // removeProduct:(productId: string) => void;
+  isOpen: boolean;
+  toggleCart: () => void;
+};
 
 export const useCartStore = create<CartState>()(
-    persist((set) => ({
-     cart:[],
-     addProduct:(product) =>
-     set((state))
-     isOpen: false,
-     toggleCart: () => set((state) => ({isOpen: !state.isOpen})),   
-    }), {name: 'cart-storage'})
+  persist(
+    (set) => ({
+      cart: [],
+      addProduct: (item) =>
+
+        set((state) => {
+          const product = state.cart.find((p) => p.id === item.id);
+          
+          if (product) {
+            
+            const updatedCart = state.cart.map((p) => {
+              if (p.id === item.id) {
+                return { ...p, quantity: p.quantity ? p.quantity + 1 : 1 };
+            }
+                return p;
+            });
+
+        return { cart: updatedCart };
+
+          } else {
+
+            return { cart: [...state.cart, { ...item, quantity: 1 }] };
+          
+         }
+        }),
+      isOpen: false,
+      toggleCart: () => set((state) => ({ isOpen: !state.isOpen })),
+    }),
+    { name: "cart-storage" }
+  )
 );
